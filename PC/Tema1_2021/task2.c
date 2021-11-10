9931a920bc8e96201c53688bc4175729f571a616
Dinicescu Radu <rodede.radu@gmail.com>
2021-11-10 11:06:56 +0200

    part 1 & 2 of task2

---
+++ PC/Tema1_2021/task2.c
@@ -0,0 +1,181 @@
+#include <stdio.h>
+#include <math.h>
+
+#define SIZE 20
+
+int gaseste_N(unsigned int instructiune)
+{
+    /*returneaza valoarea intreaga determinata de cei mai significtivi
+    3 biti ai instructiunii*/
+    unsigned int masca = 1 << 31;
+    unsigned int N = 1;
+    int i;
+
+    for (i = 0; i < 3; i++) {
+        if (masca & instructiune) {
+            switch (i) {
+            case 0:
+                N += 4;
+                break;
+            case 1:
+                N += 2;
+                break;
+            case 2:
+                N += 1;
+                break;
+            }
+        }
+        instructiune <<= 1;
+    }
+    return N;
+}
+
+void char_print_vector(char v[SIZE], int n)
+{
+    //afiseaza valorile dintr-un vector
+    int i;
+
+    for (i = 0; i < n; i++) {
+        printf("%c ", v[i]);
+    }
+}
+
+void usint_print_vector(unsigned short v[SIZE], int n)
+{
+    //afiseaza valorile dintr-un vector
+    int i;
+
+    for (i = 0; i < n; i++) {
+        printf("%hu ", v[i]);
+    }
+}
+
+void gaseste_operatii(char vector_de_operatii[8], int size, unsigned int instructiune)
+{
+    //interpreteaza urmatorii 2 * N biti ai intructiunii dupa regula data
+    instructiune <<= 3;//trecem peste primii 3 biti
+    char operatori[4] = "+-*/";
+    int i;
+    int masca = 1 << 31;
+    for (i = 0; i < size; i++) {
+        int operatie = 0;
+        if (masca & instructiune) {
+            operatie += 2;
+        }
+        if (masca & (instructiune << 1)) {
+            operatie += 1;
+        }
+        /*regula operatie:
+            0 = '+' (00)
+            1 = '-' (01)
+            2 = '*' (10)
+            3 = '/' (11)
+        */
+        vector_de_operatii[i] = operatori[operatie];
+        instructiune <<= 2;
+    }
+}
+int gaseste_dim(int N, unsigned int instructiune)
+{
+    /*returneaza valoarea intreaga determinata de cei mai putin significativ
+    4 biti ai instructiunii*/
+    int permutare_biti = 3 + 2 * N;//unde se afla cei mai putin semnificativi 4 biti
+    unsigned int i, masca = 1 << 31, dim = 1;
+    instructiune <<= permutare_biti;
+
+    for (i = 0; i < 4; i++) {
+        if (masca & instructiune) {
+            switch (i) {
+            case 0:
+                dim += 8;
+                break;
+            case 1:
+                dim += 4;
+                break;
+            case 2:
+                dim += 2;
+                break;
+            case 3:
+                dim += 1;
+                break;
+            }
+
+        }
+        instructiune <<= 1;
+    }
+    return dim;
+}
+
+unsigned short binar_in_decimal(unsigned short* numar, int dim, int* N) {
+    unsigned short masca = 1 << 15;
+    unsigned short rez = 0;
+    unsigned short copie_numar = (*numar);
+    int i;
+    
+    for (i = dim - 1; i >= 0; i--) {
+        if (masca & copie_numar) {
+            rez += pow(2, i);
+        }
+        copie_numar <<= 1;
+    }
+    (*N)--;
+    (*numar) = copie_numar;
+    return rez;
+}
+int main()
+{
+    /*interpretarea unei instructiuni date avand
+    in vedere regulile din task1 al temei 1:
+    https://ocw.cs.pub.ro/courses/programare/teme_2021/tema1_2021_cbd*/
+    unsigned int masca = 1 << 31;
+    unsigned int instructiune;
+    int i, N, dim = 1;
+    char operatii[8];
+
+
+    printf("Dati o instrctiune: ");
+    scanf("%u", &instructiune);
+
+
+    N = gaseste_N(instructiune);
+    gaseste_operatii(operatii, N, instructiune);
+    dim = gaseste_dim(N, instructiune);
+
+
+
+    printf("%d ", N);
+    char_print_vector(operatii, N);
+    printf("%d", dim);
+
+    /*task 2*/
+    unsigned short input;
+    int contor = 0, pasi_in_numar = 16 / dim;
+    unsigned short operanzi[SIZE];
+    int numar_de_operanzi;
+
+    int numar_de_inputuri = ((N + 1) * dim) / 16;
+    if ((float)numar_de_inputuri < (float)(((N + 1) * dim) / 16.0)) {
+        numar_de_inputuri++;
+    }
+    printf("\nNumar de input = %d", numar_de_inputuri);
+
+    i = 0;
+    while (numar_de_inputuri) {
+        printf("\nMai aveti de dat %d inputuri: ", numar_de_inputuri);
+        scanf("%hu", &input);
+        numar_de_inputuri--;
+
+        contor = 0;
+        while (N >= 0 && contor < pasi_in_numar) {
+            operanzi[i] = binar_in_decimal(&input, dim, &N);
+            contor++;
+            i++;
+        }
+    }
+    numar_de_operanzi = i;
+    printf("\n");
+    usint_print_vector(operanzi, numar_de_operanzi);
+
+    return 0;
+
+}
